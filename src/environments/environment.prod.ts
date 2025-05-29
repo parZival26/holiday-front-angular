0c341825afb4c4cda7cfc6a2610d1e9713998c50
Miguel <miguelangel2005.o.h@gmail.com>
2025-05-29 13:42:29 -0500

    chore: update production API URL to match new server configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000' // Cambia esto por tu URL de producción
+  apiUrl: 'http://localhost:4200' // Cambia esto por tu URL de producción
 }; 
